59d783d56d4a5f7a7c2c8d1824d1b9ccf869a645
Paul Ifemene <ifemenep@gmail.com>
2025-05-08 01:14:32 +0300

    Update lesson_one.py

--- PEACE/lesson_one.py
+++ PEACE/lesson_one.py
@@ -160,5 +160,54 @@ print(f"Your coursework is {coursework} out of 60")
 
 
    
-       
-             
+       have stored the string "Peace" in name.
+#num_one = 45
+#num_two = 78
+
+#total = num_one - num_two
+#print(float(total)) 
+
+#print("How are you",name)
+
+
+#student_name = input("Please, Enter your name here: ")
+#print("Hello", student_name, "\nHow are you \ndoing today?")
+
+#print("Welcome to Average calculator")
+#num_one = int(input("enter the first number: "))
+#num_two = int(input("enter the second number: "))
+#num_three = int(input("enter the third number: "))
+
+#average = float( (num_one + num_two + num_three) / 3)
+
+#print(f"The average is =", average)
+
+
+
+
+
+
+             have stored the string "Peace" in name.
+#num_one = 45
+#num_two = 78
+
+#total = num_one - num_two
+#print(float(total)) 
+
+#print("How are you",name)
+
+
+#student_name = input("Please, Enter your name here: ")
+#print("Hello", student_name, "\nHow are you \ndoing today?")
+
+#print("Welcome to Average calculator")
+#num_one = int(input("enter the first number: "))
+#num_two = int(input("enter the second number: "))
+#num_three = int(input("enter the third number: "))
+
+#average = float( (num_one + num_two + num_three) / 3)
+
+#print(f"The average is =", average)
+
+
+
