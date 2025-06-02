d28fcfbbdb783ebe24797f8a4281fb51b531335c
Paul Ifemene <ifemenep@gmail.com>
2025-06-02 23:03:31 +0300

    Update num_4.py

--- Gloria/num_4.py
+++ Gloria/num_4.py
@@ -7,3 +7,14 @@ def main():
         print(f"{celsius}\t{fahrenheit:.1f}")  
 
 main()
+
+
+def main():
+    print("Celsius to Fahrenheit Conversion Table")
+    print("Celsius\tFahrenheit")  
+    
+    for celsius in range(0, 101, 10):  
+        fahrenheit = 9/5 * celsius + 32
+        print(f"{celsius}\t{fahrenheit:.1f}")  
+
+main()
