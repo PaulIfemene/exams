545cf519416156c96856456e17ff335eeed31036
Paul Ifemene <ifemenep@gmail.com>
2025-06-09 02:57:10 +0300

    Update num_10.py

--- Gloria/num_10.py
+++ Gloria/num_10.py
@@ -109,3 +109,117 @@ def pounds_kilograms():
     
     print(pounds, "pounds is approximately",kilograms, "kilograms.")
 pounds_kilograms()
+
+
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
+
+
+def pounds_kilograms():
+    print("this is a Pounds to Kilograms Converter")
+    print("This program converts weights from pounds to kilograms.")
+    pounds = eval(input("Enter the weight in pounds: "))
+    kilograms = pounds * 0.453592
+    
+    print(pounds, "pounds is approximately",kilograms, "kilograms.")
+pounds_kilograms()
