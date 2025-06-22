422e090d5439fdda2ba9ddf54b726fb0ff81c653
Paul Ifemene <ifemenep@gmail.com>
2025-06-23 02:38:18 +0300

    Update num_1.py

--- Gloria/num_1.py
+++ Gloria/num_1.py
@@ -219,3 +219,666 @@ name = input("Please Enter your name here: ")
 print("Hello", name,".\nHow are you doing today?")  
 
 
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
+name = input("Please Enter your name here: ")
+print("Hello", name,".\nHow are you doing today?")  
+
+
