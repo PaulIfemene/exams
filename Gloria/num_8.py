132ed81508892a372e991df7b4bc13a4e16c1300
Paul Ifemene <ifemenep@gmail.com>
2025-07-12 23:39:50 +0300

    Update num_8.py

--- Gloria/num_8.py
+++ Gloria/num_8.py
@@ -462,3 +462,467 @@ def main():
     fahrenheit = 9/5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees fahrenheit.")
 main()
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
+
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+main()
