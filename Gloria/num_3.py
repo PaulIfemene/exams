7a1cf4ce0830e0e879293041ec3711e04e245c12
Paul Ifemene <ifemenep@gmail.com>
2025-07-22 21:25:12 +0300

    Update num_3.py

--- Gloria/num_3.py
+++ Gloria/num_3.py
@@ -66,3 +66,71 @@ def main():
         print("The temperature is", fahrenheit, "degrees Fahrenheit.")
     
 main()
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
+
+
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
+
+
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
+
+
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
+
+
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
+
+
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
+
+
+def main():
+    for i in range(5):  # Loop 5 times
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    
+main()
